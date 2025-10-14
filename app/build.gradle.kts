eb86a63fd678a3762906c2425fab9ac6cc0cd915
IamBingBing <03lonnie@naver.com>
2025-10-14 18:47:42 +0900

    init

---
+++ app/build.gradle.kts
@@ -0,0 +1,68 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+}
+
+android {
+    namespace = "com.example.studify"
+    compileSdk = 35
+    kapt  {
+        correctErrorTypes = true
+    }
+    defaultConfig {
+        applicationId = "com.example.studify"
+        minSdk = 29
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+    // retrofit2
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.6.4")
+    implementation("com.squareup.retrofit2:adapter-rxjava2:2.6.4")
+    implementation("com.google.code.gson:gson:2.8.6")
+    implementation("com.jakewharton.retrofit:retrofit2-rxjava2-adapter:1.0.0")
+    implementation("com.android.installreferrer:installreferrer:2.2")
+    // okhttp3
+    implementation("com.squareup.okhttp3:okhttp:4.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.9.0")
+    //Glide
+    implementation("com.github.bumptech.glide:glide:4.11.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.11.0")
+    //hilt
+    implementation("com.google.dagger:hilt-android:2.48")
+    kapt("com.google.dagger:hilt-android-compiler:2.48")
+    kapt("androidx.hilt:hilt-compiler:1.2.0")
+
+}
---
+++ app/src/main/java/com/example/studify/data/model/StfChatDataModel.kt
@@ -0,0 +1,14 @@
+package com.example.studify.data.model
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotation.SerializedName
+import java.io.Serializable
+
+data class StfChatDataModel (
+    @SerializedName("result_code") @Expose var resultCode : String = "",
+    @SerializedNmae
+) : Serializable {
+    data class  StfChat(
+
+    )
+}
