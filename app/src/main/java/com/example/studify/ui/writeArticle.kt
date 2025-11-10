b462774a20af7253e104c5ef18c355cf55deaa11
IamBingBing <03lonnie@naver.com>
2025-11-11 04:12:12 +0900

    매칭 옵션 설정

--- app/src/main/java/com/example/studify/ui/writeArticle.kt
+++ app/src/main/java/com/example/studify/ui/writeArticle.kt
@@ -10,7 +10,9 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -21,13 +23,14 @@ import com.example.studify.Tool.BaseModifiers
 fun writeArticle ( vm : writeArticleVM = viewModel()){
     var title by remember { mutableStateOf<String>("") }
     var content by remember { mutableStateOf<String>("") }
-    Column {
-        Row {
-            Text("제목")
-            TextField(value = title , onValueChange = {title = it  },modifier= BaseModifiers.BaseTextfillModifier )
+    Column (modifier = BaseModifiers.BaseBoxModifier){
 
-        }
+        Text("제목")
+        TextField(value = title , onValueChange = {title = it  },
+            modifier= BaseModifiers.BaseTextfillModifier.align(alignment = Alignment.CenterHorizontally) )
+
         Text("내용")
-        TextField(value= content, onValueChange = {content = it} ,modifier= BaseModifiers.BaseTextfillModifier.height(100.dp))
+        TextField(value= content, onValueChange = {content = it} ,
+            modifier= BaseModifiers.BaseTextfillModifier.height(300.dp).align(alignment = Alignment.CenterHorizontally))
     }
 }
