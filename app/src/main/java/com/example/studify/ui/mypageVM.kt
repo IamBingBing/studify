a8f9f85cac45e2516cf6ad046e4a58e38b932e7d
myoungmuk <myoungmuk819@gmail.com>
2025-11-16 23:22:09 +0900

    a
    a

--- app/src/main/java/com/example/studify/ui/mypageVM.kt
+++ app/src/main/java/com/example/studify/ui/mypageVM.kt
@@ -6,6 +6,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import com.example.studify.Tool.Preferences
 import com.example.studify.data.StudifyService
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -13,13 +14,37 @@ import javax.inject.Inject
 @HiltViewModel
 class mypageVM @Inject constructor(application: Application,studifyService: StudifyService): ViewModel() {
     var isEditing = mutableStateOf(false)
+    var name = mutableStateOf("")
+    var email = mutableStateOf("")
+    var group = mutableStateOf("")
+    var sex = mutableStateOf("")
+    var address = mutableStateOf("")
+    var tendency = mutableStateOf("")
+    var point = mutableStateOf("")
 
-    var name = mutableStateOf("강명묵")
-    var email = mutableStateOf("my@naver.com")
-    var group = mutableStateOf("A 그룹")
-    var sex = mutableStateOf("남자")
-    var address = mutableStateOf("경기도 파주시")
-    var tendency = mutableStateOf("여유로움")
-    var point = mutableStateOf("1000P")
+    init {
+        loadUserInfoFromPreferences()
+    }
+    private fun loadUserInfoFromPreferences() {
+        name.value = Preferences.getString("USERNAME") ?: ""
+        email.value = Preferences.getString("EMAIL") ?: ""
+        group.value = Preferences.getString("GROUP") ?: ""
+        address.value = Preferences.getString("ADDRESS") ?: ""
 
+        val sexInt = Preferences.getInt("SEX")
+        sex.value = when (sexInt) {
+            0 -> "남자"
+            1 -> "여자"
+            else -> "기타"
+        }
+
+        val tendencyFloat = Preferences.getFloat("TENDENCY")
+        tendency.value =
+            if (tendencyFloat == 0f) "" else "${tendencyFloat}"
+
+        val pointInt = Preferences.getInt("POINT")
+        point.value =
+            if (pointInt == 0) "0P" else "${pointInt}P"
+    }
 }
+
