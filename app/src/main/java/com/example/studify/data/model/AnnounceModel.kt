2da1837f301880c32922de55f5d26071305a92ca
soi <ramifriend6@gmail.com>
2025-11-16 22:44:33 +0900

    모델수정
    announceModel 수정

--- app/src/main/java/com/example/studify/data/model/AnnounceModel.kt
+++ app/src/main/java/com/example/studify/data/model/AnnounceModel.kt
@@ -2,15 +2,18 @@ package com.example.studify.data.model
 
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
+import java.sql.Timestamp
 
 data class AnnounceModel(
     @SerializedName ("result_code") @Expose var result_code : String?= "",
     @SerializedName("error_code") @Expose var error_code : String ?= "",
-    @SerializedName("result") @Expose var result : AnnounceContent ?=null
+    @SerializedName("result") @Expose var result : List<AnnounceContent> ?=null
 ) {
     data class AnnounceContent(
         @SerializedName("ANNOUNCE_ID") @Expose var announceid : Int? = -1,
         @SerializedName("ANNOUNCE_NAME") @Expose var announcename: String? = "",
         @SerializedName("ANNOUNCE_CONTENT") @Expose var announcecontent: String? = "",
+        @SerializedName("ANNOUNCE_DATE") @Expose var announcedate: Timestamp? = null,
+        @SerializedName("IS_PIN") @Expose var ispin: Boolean? = null,
     )
 }
