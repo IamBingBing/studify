ad47d1b875a07c2f9df853cc5b1c91442002720b
leo5869 <leo5869@naver.com>
2025-11-18 16:09:22 +0900

    qna,register,shop

--- app/src/main/java/com/example/studify/ui/qnaVM.kt
+++ app/src/main/java/com/example/studify/ui/qnaVM.kt
@@ -1,12 +1,18 @@
 package com.example.studify.ui
 
+import android.app.Application
+import androidx.compose.runtime.mutableStateMapOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import com.example.studify.data.StudifyService
+import javax.inject.Inject
 
-class qnaVM : ViewModel {
-    constructor() {
+class  qnaVM  @Inject constructor(appliction: Application, studifyService: StudifyService) : ViewModel(){
+    var qna_title =mutableStateOf<String>("")
+    var qna_id =mutableStateOf<String>("")
+    var qna_answer =mutableStateOf<String>("")
+    var qna_content =mutableStateOf<String>("")
 
-    }
-    fun requestLogin(){
+    var items = mutableStateMapOf<String, String>( "질문사항" to "질문")
 
-    }
 }
