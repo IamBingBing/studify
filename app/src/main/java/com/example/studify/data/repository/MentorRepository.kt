3956f6e240e96a2a8694143debf0d2eb3e8596a3
myoungmuk <myoungmuk819@gmail.com>
2025-11-20 19:59:33 +0900

    metor
    멘토멘토멘토멘토

--- app/src/main/java/com/example/studify/data/repository/MentorRepository.kt
+++ app/src/main/java/com/example/studify/data/repository/MentorRepository.kt
@@ -1,10 +1,22 @@
 package com.example.studify.data.repository
 
 import com.example.studify.data.StudifyService
+import com.example.studify.data.model.MentorModel
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class MentorRepository @Inject constructor(private val studifyService: StudifyService) {
-    fun request
+
+    fun requestMentorData(userId: Int): Single<MentorModel> {
+        val param = HashMap<String, String>().apply {
+            this["USERID"] = userId.toString()
+        }
+        return studifyService.requestMentorData(param)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+    }
 }
--- app/src/main/java/com/example/studify/ui/mentorVM.kt
+++ app/src/main/java/com/example/studify/ui/mentorVM.kt
@@ -5,23 +5,50 @@ import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import com.example.studify.data.StudifyService
+import com.example.studify.data.repository.MentorRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class mentorVM @Inject constructor(application: Application,studifyService: StudifyService): ViewModel() {
-    var groupName = mutableStateOf("멘토링 스터디 그룹")
-    var mentorCanTeach = mutableStateOf("나 영어 잘함")
-    var menteeWants = mutableStateOf("영어좀 알려주세요")
-    var studySchedules = mutableStateListOf<String>("이때", "모여")
-    var notices = mutableStateListOf<String>("공지", "써라")
-    var mentorList = mutableStateListOf<MentorInfo>(
-        MentorInfo(name = "멘토1", field = "백엔드 / 자료구조"),
-        MentorInfo(name = "멘토2", field = "프론트엔드 / UI/UX")
-    )
-    var menteeList = mutableStateListOf<MenteeInfo>(
-        MenteeInfo(name = "멘티1", goal = "코딩테스트 준비"),
-        MenteeInfo(name = "멘티2", goal = "안드로이드 앱 출시")
-    )
-    var currentTab = mutableStateOf(0)
+class mentorVM @Inject constructor(
+    application: Application,
+    private val mentorRepository: MentorRepository
+) : ViewModel() {
+
+    var groupName      = mutableStateOf("멘토링 스터디 그룹")
+    var mentorCanTeach = mutableStateOf("")
+    var menteeWants    = mutableStateOf("")
+    var studySchedules = mutableStateListOf<String>()
+    var notices        = mutableStateListOf<String>()
+    var mentorList     = mutableStateListOf<MentorInfo>()
+    var menteeList     = mutableStateListOf<MenteeInfo>()
+    var currentTab     = mutableStateOf(0)
+
+    init {
+        val userId = com.example.studify.Tool.Preferences.getInt("USERID")
+        if (userId != 0) {
+            loadMentorData(userId)
+        }
+    }
+
+    fun loadMentorData(userId: Int) =
+        mentorRepository.requestMentorData(userId)
+            .subscribe({ model ->
+                if (model.resultCode == "200" && model.result != null) {
+                    val r = model.result!!
+
+                    groupName.value      = "멘토링 스터디 그룹"
+                    mentorCanTeach.value = r.teach
+                    menteeWants.value    = r.qna
+
+                    mentorList.clear()
+                    mentorList.add(MentorInfo(name = r.mentor, field = r.teach))
+
+                    menteeList.clear()
+                    menteeList.add(MenteeInfo(name = r.menti, goal = r.qna))
+
+                } else {
+                }
+            }, { error ->
+            })
 }
